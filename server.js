ac2159b50f6ee1c3fe1a3b90bfd0f3080c7d4210
Remo Gonzalez <remoantonio@gmail.com>
2020-07-26 18:10:23 -0400

    here

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const userController = require('./controllers/userController.js')
 
 // Usage
 app.use(express.json())
-app.use(cors())
-// app.use(cors(corsOptions))
+// app.use(cors())
+app.use(cors(corsOptions))
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
